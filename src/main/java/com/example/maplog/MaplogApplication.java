f2131c890cb223d1e1e0b6117285592607774472
Jeongminyooa <jmjmjmz732002@naver.com>
2023-05-28 23:24:19 +0900

    initial commit

---
+++ src/main/java/com/example/maplog/MaplogApplication.java
@@ -0,0 +1,13 @@
+package com.example.maplog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaplogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaplogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/maplog/MaplogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maplog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaplogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
